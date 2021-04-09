a9c84f8ab8e7c843a7668e569ee2a10fc2e3678e
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-09 00:28:57 +0000

    Fetch the next ISS flyovers for our geo coordinates

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // index.js
 const { fetchMyIP } = require('./iss');
 
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+
 fetchMyIP((error, ip) => {
   
   if (error) {
--- iss.js
+++ iss.js
@@ -17,6 +17,27 @@ const fetchMyIP = function(callback) {
 })
 }
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+// Don't need to export the other function since we are not testing it right now.
+module.exports = { fetchCoordsByIP };
+
 module.exports = { fetchMyIP };
 
 /**
