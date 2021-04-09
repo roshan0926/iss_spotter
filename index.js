05b9cd1dfe83e52716b93f0d17bcc54602edd508
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-09 00:31:40 +0000

    added flyiver times

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-// index.js
-const { fetchMyIP } = require('./iss');
 
-const { fetchCoordsByIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
 
-fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+// const { fetchCoordsByIP } = require('./iss');
+
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
 
-fetchMyIP((error, ip) => {
+// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
+
+// fetchMyIP((error, ip) => {
   
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  } 
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   } 
 
-  console.log('It worked! Returned IP:' , ip);
-});
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
 
  
--- iss.js
+++ iss.js
@@ -1,44 +1,62 @@
 const request = require('request')
 
-const fetchMyIP = function(callback) { 
-  request(`https://api.ipify.org/?format=json`, (error, response, body) => {
-  if (error) {
-    callback(error, null);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.log(body)
-    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-    callback(Error(msg), null);
-    return;
-  }
-  const ip = JSON.parse(body).ip;
-    callback(null, ip);
-})
-}
+// const fetchMyIP = function(callback) { 
+//   request(`https://api.ipify.org/?format=json`, (error, response, body) => {
+//   if (error) {
+//     callback(error, null);
+//     return;
+//   }
+//   if (response.statusCode !== 200) {
+//     console.log(body)
+//     const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//     callback(Error(msg), null);
+//     return;
+//   }
+//   const ip = JSON.parse(body).ip;
+//     callback(null, ip);
+// })
+// }
 
-const fetchCoordsByIP = function(ip, callback) {
-  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+// const fetchCoordsByIP = function(ip, callback) {
+//   request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
+
+//     if (response.statusCode !== 200) {
+//       callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+//       return;
+//     }
+
+//     const { latitude, longitude } = JSON.parse(body);
+
+//     callback(null, { latitude, longitude });
+//   });
+// };
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
 
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
       return;
     }
 
-    const { latitude, longitude } = JSON.parse(body);
-
-    callback(null, { latitude, longitude });
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
   });
 };
 
-// Don't need to export the other function since we are not testing it right now.
-module.exports = { fetchCoordsByIP };
-
-module.exports = { fetchMyIP };
+module.exports = { fetchISSFlyOverTimes };
+// module.exports = { fetchCoordsByIP };
+// module.exports = { fetchMyIP };
 
 /**
  * Makes a single API request to retrieve the user's IP address.
